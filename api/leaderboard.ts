89ca64a82c3fa4e9e26d556686de4621f84739cc
dauntless-arcane <tharunjthegreat@gmail.com>
2026-02-09 08:22:24 +0530

    Update import statements for story.json to use type annotations

--- api/leaderboard.ts
+++ api/leaderboard.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const filePath = path.join(process.cwd(), "api/scores.json");
 
-export default function handler(req: any, res: any) {
+export default function handler(_req: any, res: any) {
   if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, "[]");
 
   const scores = JSON.parse(fs.readFileSync(filePath, "utf-8"));
