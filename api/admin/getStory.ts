1ef60f6250c23475892cedebeda9625ff3d21326
dauntless-arcane <tharunjthegreat@gmail.com>
2026-02-09 19:38:43 +0530

    Refactor: correct path

--- api/admin/getStory.ts
+++ api/admin/getStory.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export default function getStory() {
-  const activePath = path.join(process.cwd(), "api/activeStory.json");
+  const activePath = path.join(process.cwd(), "./activeStory.json");
   const active = JSON.parse(fs.readFileSync(activePath, "utf-8"));
 
   const storyPath = path.join(
